7979d6dffb8657514abef16394e3396b6b28086c
Jun <32525718+monuda85@users.noreply.github.com>
2019-02-04 19:52:32 -0800

    Implemented Search Function

--- js/comps.js
+++ js/comps.js
@@ -9,7 +9,7 @@ Vue.component('ingredientList', {
 Vue.component('search', {
 	template:'\
 	<form class="form-inline">\
-		<div class="input-group">\
+		<div class="input-group input-group-lg"">\
 			<input class="form-control py-2 border-right-0 border" type="search" id="example-search-input" placeholder="Search your craves">\
 		<span class="input-group-append">\
 					<div class="input-group-text bg-transparent">\
