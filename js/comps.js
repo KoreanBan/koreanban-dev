6f04c37ae162d7573c62c3744407e2af3a1ad27f
Jun Jung <a01039273@dtc01l0870-25.ad.bcit.ca>
2019-03-09 19:13:17 -0800

    Modal 2 in progress

--- js/comps.js
+++ js/comps.js
@@ -45,30 +45,49 @@ Vue.component('cuismodal',{
 
 Vue.component('cuisedit', {
   props:['obj2'],
-  template:'<div>\
-  <button>Close</button>\
-  <p>Food Name:</p>\
-  <input type="text" v-bind:value="obj2.food_name" v-on:change="$emit(\'change\', $event.target.value)"/>\
-  <p>Food Image URL:</p>\
-  <input type="text" v-bind:value="obj2.food_image" v-on:change="$emit(\'change\', $event.target.value)"/>\
-  <p>Food Description:</p>\
-  <input type="text" v-bind:value="obj2.food_desc" v-on:change="$emit(\'change\', $event.target.value)"/>\
-  <p>Food Origin:</p>\
-  <input type="text" v-bind:value="obj2.food_origin" v-on:change="$emit(\'change\', $event.target.value)"/>\
-  <p>Google Map URL:</p>\
-  <input type="text" v-bind:value="obj2.food_url" v-on:change="$emit(\'change\', $event.target.value)"/>\
-  <p>Food Ingredients:</p>\
-  <span v-for="oj in obj2.food_ingredients">\
-  <input type="text" v-bind:value="oj.list" v-on:change="$emit(\'change\', $event.target.value)"/>\
-  <input type="text" v-bind:value="oj.quantity" v-on:change="$emit(\'change\', $event.target.value)"/>\
-  <br/>\
-  </span>\
-  <p>Food Recipes:</p>\
-  <span v-for="oj in obj2.food_recipes">\
-  <input type="text" v-bind:value="oj.list" v-on:change="$emit(\'change\', $event.target.value)"/>\
-  <br/>\
-  </span>\
-  <button @click="$emit(\'some-change\')">Update</button>\
-  </div>'
+  template:
+  '<transition name="fade">\
+    <form class="wrapper_admin">\
+      <div class="form-group">\
+        <label>Food Name:</label>\
+        <input class="form-control" type="text" v-bind:value="obj2.food_name" v-on:change="$emit(\'change\', $event.target.value)"/>\
+      </div>\
+      <div class="form-group">\
+        <label>Food Image URL:</label>\
+        <input class="form-control" type="text" v-bind:value="obj2.food_image" v-on:change="$emit(\'change\', $event.target.value)"/>\
+      </div>\
+      <div class="form-group">\
+        <label>Food Description:</label>\
+        <input class="form-control" type="text" v-bind:value="obj2.food_desc" v-on:change="$emit(\'change\', $event.target.value)"/>\
+      </div>\
+      <div class="form-group">\
+        <label>Food Origin:</label>\
+        <input class="form-control" type="text" v-bind:value="obj2.food_origin" v-on:change="$emit(\'change\', $event.target.value)"/>\
+      </div>\
+      <div class="form-group">\
+        <label>Google Map URL:</label>\
+        <input class="form-control" type="text" v-bind:value="obj2.food_url" v-on:change="$emit(\'change\', $event.target.value)"/>\
+      </div>\
+      <div class="form-group">\
+        <label>Food Ingredients:</label> <br/>\
+        <span v-for="oj in obj2.food_ingredients">\
+          <input type="text" v-bind:value="oj.list" v-on:change="$emit(\'change\', $event.target.value)"/>\
+          <input type="text" v-bind:value="oj.quantity" v-on:change="$emit(\'change\', $event.target.value)"/>\
+        <br/>\
+        </span>\
+      </div>\
+      <div class="form-group">\
+        <label>Food Recipes:</label>\
+        <span v-for="oj in obj2.food_recipes">\
+          <input class="form-control" type="text" v-bind:value="oj.list" v-on:change="$emit(\'change\', $event.target.value)"/>\
+          <br/>\
+        </span>\
+      </div>\
+      <span class="modal-footer">\
+        <button class="btn" @click="$emit(\'some-change\')">Update</button>\
+        <button class="btn">Close</button>\
+      </span>\
+    </form>\
+  </transition>' 
 });
 
